53c7423e4d9df7a447e45259af6dcffbcb20338b
GhalibAansariSpacecode <65384680+GhalibAansariSpacecode@users.noreply.github.com>
2020-05-17 00:52:59 +0530

    add address done, update address and address model done.

---
+++ app/controller/address.controller.js
@@ -0,0 +1,72 @@
+const {Request, Response, Router} = require("express");
+const Joi = require('joi')
+const {guard} = require("../helper/Auth");
+const {addressModel} = require('../model/address.model');
+const {Constant} = require('../constant');
+
+const {passwordRegex, emailRegex} = Constant;
+
+//init router instance.
+const router = Router();
+
+
+let addAddress = (req, res) => {
+    const schema = Joi.object().keys({
+        address1: Joi.string().required(),
+        address2: Joi.string().required(),
+        city: Joi.string().required(),
+        country: Joi.string().required(),
+        pin_zip: Joi.string().required(),
+        loggedInUser: Joi.any(),
+    });
+    const newData = req["body"];
+    try {
+        Joi.validate(newData, schema, async (err, value) => {
+            if (err) { res.status(400).json({status: 400, success: false, message: err.details[0].message}) }
+            else {
+                newData.createdBy = req["body"].loggedInUser._id;
+                newData.modifiedBy = req["body"].loggedInUser._id;
+                let data = await addressModel(newData).save().catch((err) => {
+                    return res.status(400).json({status: 400, success: false, message: err["errmsg"] || err['message']})
+                });
+                await res.status(200).json({status: 200, success: true, message: "Role type Successfully saved.", data})
+            }
+        })
+    } catch (err) {
+        res.status(400).json({status: 400, success: false, message: err.message,})
+    }
+};
+
+let updateAddress = (req, res) => {
+    const schema = Joi.object().keys({
+        id: Joi.string().required(),
+        address1: Joi.string().required(),
+        address2: Joi.string().required(),
+        city: Joi.string().required(),
+        country: Joi.string().required(),
+        pin_zip: Joi.string().required(),
+        loggedInUser: Joi.any(),
+    });
+    const newData = req["body"];
+    try {
+        Joi.validate(newData, schema, async (err, value) => {
+            if (err) res.status(400).json({status: 400, success: false, message: err.details[0].message})
+            else {
+                newData.modifiedBy = req["body"].loggedInUser._id;
+                let data = await addressModel.findByIdAndUpdate(newData.id, newData).catch((err) => {
+                    return res.status(400).json({status: 400, success: false, message: err["errmsg"] || err['message']})
+                });
+                await res.status(200).json({status: 200, success: true, message: "Role type Successfully updated.", data: data})
+            }
+        })
+    } catch (err) {
+        res.status(400).json({status: 400, success: false, message: err.message,})
+    }
+};
+
+
+//all sub-route's.
+router.post('/add', guard, addAddress);
+router.put('/update', guard, updateAddress);
+
+exports.addressRouter = router;
--- app/router/index.js
+++ app/router/index.js
@@ -1,7 +1,8 @@
+const {Router} = require('express')
+const {addressRouter} = require("../controller/address.controller");
 const {roleRouter} = require("../controller/role.controller");
 const {contactRouter} = require("../controller/contact.controller");
 const {modelRouter} = require("../controller/model.controller");
-const {Router} = require('express')
 const {userRouter} = require('../controller/user.controller')
 const {roleTypeRouter} = require('../controller/role_type.controller')
 
@@ -14,6 +15,7 @@ router.use('/roleType', roleTypeRouter);
 router.use('/model', modelRouter);
 router.use('/contact', contactRouter);
 router.use('/role', roleRouter);
+router.use('/address', addressRouter);
 //404 router here.
 router.use('*', function (req, res) { res.status(404).json({status: 404, success: false, message: "Page not found."}) });   //404 pages here.
 
